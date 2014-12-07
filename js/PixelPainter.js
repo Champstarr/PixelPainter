8140b9d5982ce24fe15b00b89dec8879fc847ef0
Andrew Taylor <Winstonia@Andrews-MacBook-Pro.local>
2014-12-06 20:17:55 -1000

    changes to pixel painter

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 	function PixelPainter(width,height) {
+    var colorPalette = [
+      ["red", "blue", "green"],
+      ["purple", "orange", "yellow"]
+    ];
     this.controls = $('#controls');
     this.artboard = $('#artboard');
-    for (var i=0; i<height; i++) {
+    for (var i=0; i<height; i++) { //for grid
       var row = $('<div class="row clearfix">')
       
       for (var j=0; j<width; j++) {
@@ -12,16 +16,26 @@ $(document).ready(function(){
       row.appendTo('#artboard');
 
     }
-
-    for (var i=0; i<height; i++) {
-      var row = $('<div class="row clearfix">')
+    //This is for the color palatte222
+    for (var i=0; i<2; i++) {
+      var row2 = $('<div>', {
+        class: "row2 clearfix"
+      });
       
-      for (var j=0; j<width; j++) {
-        var col = $('<div class="col">')
-        row.append(col);  
-      };
-      row.appendTo('#artboard');
+      for (var j=0; j<3; j++) {
+        var col2 = $('<div>',{
+          class: "col2 clearfix",
+          css:{
+            "background-color": colorPalette[i][j]
+          }
+        });
 
+        row2.append(col2);  
+      };
+      row2.appendTo('#controls');
+    }
+   var cf = $('<div class="clearfix">');
+   cf.insertAfter('#artboard');
 
     
 	} // ends PixelPainter class
